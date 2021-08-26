Fixed normal calculation, fixed creation of points in 3D space, fixed reasoning about coplanarity.

assemble_objects.py
@@ -107,7 +107,7 @@ def determine_neighbors_with_border_centers_calc(surfaces, number_of_surfaces, d
 
     return neighbors_list, border_centers
 
-def determine_centroids(depth_image, positions, average_positions, points_in_space):
+def determine_centroids(positions, average_positions, points_in_space):
     for i in range(len(positions)):
         del positions[i][0]
     positions = [np.asarray(p) for p in positions]
@@ -226,7 +226,7 @@ def swap_values(vec):
 def determine_convexity_with_closest_points(angles, closest_points, neighbors, number_of_surfaces, depth_image, join_matrix, surfaces):
     convexity = np.zeros((number_of_surfaces, number_of_surfaces))
     concave = np.zeros((number_of_surfaces, number_of_surfaces))
-
+    new_surfaces = surfaces.copy()
     for i in range(number_of_surfaces):
         for j in range(len(neighbors[i])):
             surface_2 = neighbors[i][j]
@@ -289,7 +289,6 @@ def determine_convexity_with_closest_points(angles, closest_points, neighbors, n
                 # for m in range(length + 1):
                 #     point = c_1 + m * direction
                 #     surfaces[int(point[1])][int(point[0])] = 0
-
     join_matrix = join_matrix + convexity
     join_matrix[join_matrix > 1] = 1
     return join_matrix, concave
@@ -355,6 +354,8 @@ def color_and_angle_histograms(lab_image, angle_image, surfaces, number_of_surfa
     inverse_pixels = 1 / pixels_per_surface * 10
     height, width = np.shape(surfaces)
     counts = np.zeros(number_of_surfaces)
+    all_angles_sum = np.zeros((number_of_surfaces, 2))
+    patch_counter = np.zeros(number_of_surfaces)
     for y in range(height):
         for x in range(width):
 
@@ -368,21 +369,29 @@ def color_and_angle_histograms(lab_image, angle_image, surfaces, number_of_surfa
             histograms_angles[a_1][a_2][s] += inverse_pixels[s]
             histograms_color[int(a)][int(b)][s] += inverse_pixels[s]
 
+            s = patches[y][x]
+            if s < 0.0001:
+                continue
+
+            all_angles_sum[s] += angle_image[y][x]
+            patch_counter[s] += 1
+
             if y % 4 == 0 and x % 4 == 0:
-                s = patches[y][x]
-                if s < 0.0001:
-                    continue
                 new_x = int(x/4)
                 new_y = int(y/4)
                 texture = texture_image[new_y][new_x]
                 histograms_texture[s] += texture
                 counts[s] += 1
+
+    patch_counter[patch_counter == 0] = 1
+    all_angles_sum = all_angles_sum / np.expand_dims(patch_counter, axis=-1)
+
     histograms_texture = histograms_texture / np.expand_dims(counts, axis=-1)
     histograms_texture[np.isnan(histograms_texture)] = 0
 
     histograms_texture = histograms_texture/np.sum(histograms_texture, axis=0, keepdims=True) * 10
     histograms_texture[np.isnan(histograms_texture)] = 0
-    return histograms_color, histograms_angles, histograms_texture
+    return histograms_color, histograms_angles, histograms_texture, all_angles_sum
 
 def chi_squared_distances_model(pool_size=(5, 5), strides=(2, 2)):
     input = layers.Input(shape=(None, None, None))
@@ -523,9 +532,10 @@ def find_even_planes_and_most_common_normal(angle_histogram, number_of_surfaces)
     normals = np.zeros((number_of_surfaces, 2), dtype="float32")
     for i in range(number_of_surfaces):
         indices = [[0, 0]]
+        max = np.max(angle_histogram[i])
         for j in range(l):
             for k in range(l):
-                if angle_histogram[i][j][k] > 0.7:
+                if angle_histogram[i][j][k] == max:
                     indices.append([j, k])
         del indices[0]
         if len(indices) == 0:
@@ -546,9 +556,9 @@ def find_even_planes_and_most_common_normal(angle_histogram, number_of_surfaces)
             normals[i] = np.asarray([n_1, n_2])
     return planes, normals
 
-def determine_coplanarity(similarities, angle_similarities, number_of_surfaces, centroids, angle_histogram):
+def determine_coplanarity(similarities, angle_similarities, number_of_surfaces, centroids, angle_histogram, average_normals):
     planes, normals = find_even_planes_and_most_common_normal(np.moveaxis(angle_histogram, 2, 0), number_of_surfaces)
-    normal_vectors = angles_to_normals(normals)
+    normal_vectors = angles_to_normals(average_normals)
     coplanarity = np.zeros((number_of_surfaces, number_of_surfaces))
     for i in range(number_of_surfaces):
         if planes[i] > 0:
@@ -557,20 +567,20 @@ def determine_coplanarity(similarities, angle_similarities, number_of_surfaces, 
                     diff = (lambda x: x/np.linalg.norm(x))(centroids[i] - centroids[j])
                     val_1 = np.abs(np.dot(diff, normal_vectors[i]))
                     val_2 = np.abs(np.dot(diff, normal_vectors[j]))
-                    if angle_similarities[i][j] < 1 and np.abs(np.dot(diff, normal_vectors[i])) < 0.1 and np.abs(np.dot(diff, normal_vectors[j])) < 0.1:
+                    if angle_similarities[i][j] < 1.2 and np.abs(np.dot(diff, normal_vectors[i])) < 0.1 and np.abs(np.dot(diff, normal_vectors[j])) < 0.1:
                         coplanarity[i][j] = 1
                         coplanarity[j][i] = 1
     return coplanarity
 
 def extract_information(rgb_image, texture_model, surfaces, patches, number_of_surfaces, normal_angles, lab_image, depth_image, points_in_space):
     average_positions, counts, positions = calc_average_position_and_counts(surfaces, number_of_surfaces)
-    centroids = determine_centroids(depth_image, positions, average_positions, points_in_space)
+    centroids = determine_centroids(positions, average_positions, points_in_space)
     texture = texture_image(rgb_image, texture_model)
-    histogram_color, histogram_angles, histogram_texture = color_and_angle_histograms(lab_image, normal_angles,
+    histogram_color, histogram_angles, histogram_texture, average_normals = color_and_angle_histograms(lab_image, normal_angles,
                                                                                       surfaces, number_of_surfaces,
                                                                                       counts, texture, patches)
     depth_edges = find_edges.find_edges_from_depth_image(depth_image)
-    return average_positions, histogram_color, histogram_angles, histogram_texture, depth_edges, centroids
+    return average_positions, histogram_color, histogram_angles, histogram_texture, depth_edges, centroids, average_normals
 
 def determine_convexly_connected_surfaces(nearest_points_func, surface_patch_points, neighbors, border_centers, normal_angles, number_of_surfaces, depth_image, join_matrix, surfaces):
     nearest_points = nearest_points_func(surface_patch_points, prepare_border_centers(neighbors, border_centers))
@@ -605,12 +615,9 @@ def texture_similarity_calc(texture_vecs, number_of_surfaces):
     return np.sqrt(np.sum(np.square(diff), axis=-1))
 
 def assemble_surfaces(surfaces, normal_angles, rgb_image, lab_image, depth_image, number_of_surfaces, patches, models, vectors, points_in_space):
-    #plot_image.plot_normals(np.reshape(angles_to_normals(np.reshape(normal_angles, (480*640, 2))),(480, 640, 3)))
-    #quit()
-    plot_surfaces(surfaces, False)
     color_similarity_model, angle_similarity_model, texture_similarity_model, texture_model, nearest_points_func = models
     plot_surfaces(surfaces, False)
-    average_positions, histogram_color, histogram_angles, histogram_texture, depth_edges, centroids \
+    average_positions, histogram_color, histogram_angles, histogram_texture, depth_edges, centroids, average_normals \
         = extract_information(rgb_image, texture_model, surfaces, patches, number_of_surfaces, normal_angles, lab_image, depth_image, points_in_space)
 
     color_similarities = color_similarity_model(histogram_color)
@@ -621,8 +628,8 @@ def assemble_surfaces(surfaces, normal_angles, rgb_image, lab_image, depth_image
     surface_patch_points = extract_points(patches, number_of_surfaces)
 
     similarity_neighbors, similarity_neighbors_concave, similarity_occlusion, similarity_occlusion_coplanar\
-        = determine_similar_patches(texture_similarities, color_similarities, angle_similarities, [(9, 0.6), (5, 0.4), (7, 0.5, 1.1), (12, 1.1, 1.1)], neighbors)
-    coplanarity = determine_coplanarity(similarity_occlusion_coplanar, angle_similarities, number_of_surfaces, centroids, histogram_angles)
+        = determine_similar_patches(texture_similarities, color_similarities, angle_similarities, [(9, 0.6), (5, 0.4), (7, 0.5, 1.3), (12, 1.1, 1.3)], neighbors)
+    coplanarity = determine_coplanarity(similarity_occlusion_coplanar, angle_similarities, number_of_surfaces, centroids, histogram_angles, average_normals)
 
     join_matrix = np.zeros((number_of_surfaces, number_of_surfaces))
     join_matrix, concave = determine_convexly_connected_surfaces(nearest_points_func, surface_patch_points, neighbors,
@@ -648,8 +655,6 @@ def main():
         patches = np.load(f"out/{index}/patches.npy")
         vectors = np.load(f"out/{index}/vectors.npy")
         points_in_space = np.load(f"out/{index}/points.npy")
-        load_pcd.create_pcd_from_points(points_in_space, rgb, vectors)
-        quit()
         Q = np.argmax(Q, axis=-1)
         number_of_surfaces = int(np.max(Q) + 1)
         assemble_surfaces(Q, angles, rgb, lab, depth_image, number_of_surfaces, patches, models, vectors, points_in_space)

image_processing_models_GPU.py
@@ -424,16 +424,15 @@ def normals_and_log_depth_model_GPU(pool_size=2, height=height, width=width):
     p = pool_size*2+1
 
     x_val = tf.constant(viewing_angle_x/2, dtype="float32")
-    y_val = tf.constant(viewing_angle_y/4, dtype="float32")
+    y_val = tf.constant(viewing_angle_y/2, dtype="float32")
 
     x_list = tf.scalar_mul(x_val, tf.subtract(tf.divide(tf.range(0, width, 1, dtype="float32"), width/2 - 0.5), 1))
-    y_list = tf.scalar_mul(y_val, tf.subtract(tf.divide(tf.range(0, height, 1, dtype="float32"), height/2 - 0.5), 1))
+    y_list = tf.scalar_mul(y_val, tf.subtract(tf.divide(tf.range(height-1, -1, -1, dtype="float32"), height/2 - 0.5), 1))
 
     x_array = tf.expand_dims(tf.tile(tf.expand_dims(x_list, axis=0), [height, 1]), axis=-1)
     y_array = tf.expand_dims(tf.tile(tf.expand_dims(y_list, axis=1), [1, width]), axis=-1)
-    #angle_values_depth = tf.math.cos(tf.sqrt(tf.reduce_sum(tf.square(tf.concat([y_array, x_array], axis=-1)))))
-    angle_values_x = tf.ones_like(x_array)
-    angle_values_y = tf.ones_like(y_array)
+    angle_values_x = tf.math.tan(x_array)
+    angle_values_y = tf.math.tan(y_array)
 
     depth_image = layers.Input(batch_shape=(1, height, width), dtype=tf.float32)
     depth_image_expanded = tf.expand_dims(depth_image, axis=-1)
@@ -447,25 +446,25 @@ def normals_and_log_depth_model_GPU(pool_size=2, height=height, width=width):
     y_array = angle_values_y*smoothed_depth
     points_in_space = layers.Concatenate()([x_array, y_array, smoothed_depth])
 
-    factor_x_in = layers.Input(shape=(1,), dtype=tf.float32)
-    factor_y_in = layers.Input(shape=(1,), dtype=tf.float32)
+    # factor_x_in = layers.Input(shape=(1,), dtype=tf.float32)
+    # factor_y_in = layers.Input(shape=(1,), dtype=tf.float32)
+    #
+    # zeros = tf.zeros(tf.shape(smoothed_depth))
+    # x_values = tf.multiply(tf.broadcast_to(factor_x, tf.shape(smoothed_depth)), smoothed_depth)
+    # y_values = tf.multiply(tf.broadcast_to(factor_y, tf.shape(smoothed_depth)), smoothed_depth)
 
-    zeros = tf.zeros(tf.shape(smoothed_depth))
-    x_values = tf.multiply(tf.broadcast_to(factor_x, tf.shape(smoothed_depth)), smoothed_depth)
-    y_values = tf.multiply(tf.broadcast_to(factor_y, tf.shape(smoothed_depth)), smoothed_depth)
-
-    pad_1 = tf.pad(smoothed_depth, [[2, 0], [0, 0], [0, 0]])
-    pad_2 = tf.pad(smoothed_depth, [[0, 2], [0, 0], [0, 0]])
-    pad_3 = tf.pad(smoothed_depth, [[0, 0], [2, 0], [0, 0]])
-    pad_4 = tf.pad(smoothed_depth, [[0, 0], [0, 2], [0, 0]])
+    pad_1 = tf.pad(points_in_space, [[2, 0], [0, 0], [0, 0]])
+    pad_2 = tf.pad(points_in_space, [[0, 2], [0, 0], [0, 0]])
+    pad_3 = tf.pad(points_in_space, [[0, 0], [2, 0], [0, 0]])
+    pad_4 = tf.pad(points_in_space, [[0, 0], [0, 2], [0, 0]])
 
     sub_1 = tf.subtract(pad_1, pad_2)[1:-1, :, :]
     sub_2 = tf.subtract(pad_4, pad_3)[:, 1:-1, :]
 
-    concat_1 = layers.Concatenate(axis=-1)([zeros, y_values, sub_1])
-    concat_2 = layers.Concatenate(axis=-1)([x_values, zeros, sub_2])
+    #concat_1 = layers.Concatenate(axis=-1)([zeros, y_values, sub_1])
+    #concat_2 = layers.Concatenate(axis=-1)([x_values, zeros, sub_2])
 
-    vectors = tf.linalg.cross(concat_1, concat_2)
+    vectors = tf.linalg.cross(sub_1, sub_2)
     condition = -tf.cast(tf.greater(tf.gather(vectors, [2], axis=-1), 0), tf.float32)
     vectors = 2*tf.multiply(tf.broadcast_to(condition, tf.shape(vectors)), vectors) + vectors
 
@@ -475,8 +474,8 @@ def normals_and_log_depth_model_GPU(pool_size=2, height=height, width=width):
     angles = Calc_Angles()(normals)
     smoothed_angles = uniform_filter_with_depth_cutoff(tf.expand_dims(angles, axis=0), log_image, [1, p, p, 1], int((p**2-1)/2), 2, p**2)
 
-    model = Model(inputs=[depth_image, factor_x_in, factor_y_in], outputs=[tf.squeeze(tf.squeeze(log_image, axis=-1), axis=0), smoothed_angles, normals, points_in_space])
-    return lambda image: model.predict([np.asarray([image]), np.asarray([2*factor_x]), np.asarray([2*factor_y])], batch_size=1)
+    model = Model(inputs=depth_image, outputs=[tf.squeeze(tf.squeeze(log_image, axis=-1), axis=0), smoothed_angles, normals, points_in_space])
+    return lambda image: model.predict(np.asarray([image]), batch_size=1)
 
 
 def find_surfaces_model_GPU(depth=4, factor_array=None, threshold=0.007003343675404672 * 10, height=height, width=width, alpha=6, s1=2, s2=1, n1=11, n2=5, component_threshold=20):